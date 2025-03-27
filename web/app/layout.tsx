cc97020e2941f4c504182dd74a4df606d1be3b75
Marc <marc.blattmann08@gmail.com>
2025-03-27 08:21:58 +0100

    Small Fixes | Quality of life

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,6 +1,8 @@
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -31,9 +33,14 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <main className="h-screen flex flex-col items-center">
-            {children}
-          </main>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <main className="h-screen flex flex-col">
+                {children}
+              </main>
+            </SidebarInset>
+          </SidebarProvider>
         </ThemeProvider>
       </body>
     </html>
--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,18 +1,13 @@
 import Footer from "@/components/footer";
 import NavBar from "@/components/navBar";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { Button } from "@/components/ui/button";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
-import Link from "next/link";
 
 export default async function Home() {
   return (
     <>
       <NavBar/>
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        Main Page
-      </main>
+        <main className="flex-1 flex flex-col gap-6 px-4">
+          Main Page
+        </main>
       <Footer/>
     </>
   );
