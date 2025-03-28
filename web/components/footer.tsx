d57c89506477ab598235ec04440292db5abc9987
Marc <marc.blattmann08@gmail.com>
2025-03-28 09:56:49 +0100

    Better footer

--- web/components/footer.tsx
+++ web/components/footer.tsx
@@ -1,9 +1,14 @@
 export default function Footer() {
     return(
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-10">
-            <p>
-                EduSync
-            </p>
+            <div className="flex flex-col items-center justify-center">
+                <p>
+                    © 2025 EduSync. All rights reserved.
+                </p>
+                <p>
+                    Developed with ❤️ by the EduSync Team
+                </p>
+            </div>
         </footer>
     );
 }
