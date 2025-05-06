9ee59c87194b1eecedc99c26a4e77c00ac060ddb
Stardo <marc.blattmann08@gmail.com>
2025-05-06 12:28:25 +0200

    fixxxx

--- web/utils/stripe/client.ts
+++ web/utils/stripe/client.ts
@@ -6,7 +6,7 @@ if (!process.env.STRIPE_SECRET_KEY) {
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16',
+  apiVersion: '2025-04-30.basil',
 });
 
 export default stripe;
