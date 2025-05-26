771b8c00bdf328fa6a079843ecf38b450df8f61f
Stardo <marc.blattmann08@gmail.com>
2025-05-26 20:55:58 +0200

    Removed Everything

--- web/components/navBar.tsx
+++ web/components/navBar.tsx
@@ -4,11 +4,12 @@ import { ThemeSwitcher } from "./theme-switcher";
 
 export default function NavBar() {
     return(
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <nav className="w-full flex justify-center h-16">
             <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 text-lg items-center font-semibold">
                 <Link href="/">EduSync</Link>
-            </div>            <div className="flex gap-5 items-center">
+            </div>            
+            <div className="flex gap-5 items-center">
                 <ThemeSwitcher/>
                 <Button asChild size="sm" variant={"default"}>
                 <Link href="/sign-in">Sign in with Google</Link>
